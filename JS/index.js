9d0ce00914a3c18a66cc6036c1fb8de8c7259756
Dean Dadalt <94490399+WarriorAttHell@users.noreply.github.com>
2023-10-01 20:49:18 -0300

    Delete JS directory

--- JS/index.js
+++
@@ -1,8 +0,0 @@
-// Simulação de adicionar ao carrinho
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
-
-addToCartButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Produto adicionado ao carrinho!');
-    });
-});
